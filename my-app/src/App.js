621e298ed8ae5e0f658cc2db8b3dac18cabc6896
Rely Ky <relyky@hotmail.com>
2022-03-14 23:47:14 +0800

    Home.js

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,26 +1,10 @@
-import logo from './logo.svg';
-import './App.css';
+import Home from './Home'
 
-function App() {
+export default function App() {
   return (
-    <div className="App">
-      <h1>Go Go</h1>
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>App Banner</h1>
+      <Home />
     </div>
   );
 }
-
-export default App;
