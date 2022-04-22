1c6cb99a45b72a2a169b962936ac6b36b64f1db9
Rely Ky <relyky@hotmail.com>
2022-04-22 23:25:46 +0800

    Flow sample 1

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 //import { useState } from 'react'
 import { Routes, Route, Link as RouterLink } from 'react-router-dom'
-import { AppBar, Toolbar, Divider, Typography, Stack, Link, Button } from '@mui/material'
+import { AppBar, Toolbar, Divider, Typography, Stack, Link } from '@mui/material'
 import Home from './Home'
 import AP0101 from './AppForm/AP0101/appCtx'
 import AP0102 from './AppForm/AP0102/appCtx'
@@ -15,7 +15,7 @@ export default function App() {
           </Typography>
 
           <Stack direction="row" spacing={1} divider={<Divider orientation="vertical" flexItem color="whitesmoke" />} >
-            <Link component={RouterLink} to="/" hover>
+            <Link component={RouterLink} to="/">
               <Typography variant="body1" color="white">Home</Typography>
             </Link>
             <Link component={RouterLink} to="/ap0101">
--- src/AppForm/AP0101/appCtx.js
+++ src/AppForm/AP0101/appCtx.js
@@ -1,7 +1,12 @@
 import React, { useEffect } from 'react'
+import Flow from './Flow'
 
 export default function AppCtx() {
   return (
-    <h1>AP0101</h1>
+    <div>
+      <h1>AP0101</h1>
+      <Flow/>
+      <p>footer</p>
+    </div>
   )
 }
